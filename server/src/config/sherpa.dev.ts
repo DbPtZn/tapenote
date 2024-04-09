6231d90820abbd5c0f403139e097aa471d8c0bf8
DbPtZn <261849747@qq.com>
2024-04-09 14:38:29 +0800

    好像什么都没改

--- server/src/config/sherpa.dev.ts
+++ server/src/config/sherpa.dev.ts
@@ -17,7 +17,7 @@ export default registerAs('sherpa', () => ({
         debug: 1,
         provider: 'cpu'
       },
-      ruleFsts: 'sherpa/vits-zh-hf-fanchen-C/rule.fst',
+      ruleFars: 'sherpa/vits-zh-hf-fanchen-C/rule.fst',
       maxNumSentences: 1
     },
     asrConfig: {
