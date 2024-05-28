02dae81d6e60daf4f3e03d96818e5351a0e6c790
DbPtZn <261849747@qq.com>
2024-05-28 22:49:33 +0800

    build 2024.5.28

--- server/src/config/sherpa.prod.ts
+++ server/src/config/sherpa.prod.ts
@@ -7,22 +7,25 @@ export default registerAs('sherpa', () => ({
   },
   offline: {
     ttsConfig: {
-      offlineTtsModelConfig: {
-        offlineTtsVitsModelConfig: {
-          model: 'sherpa//vits-zh-hf-fanchen-C/vits-zh-hf-fanchen-C.onnx',
-          lexicon: 'sherpa/vits-zh-hf-fanchen-C/lexicon.txt',
+      model: {
+        vits: {
+          model: 'sherpa/vits-zh-hf-fanchen-C/vits-zh-hf-fanchen-C.onnx',
           tokens: 'sherpa/vits-zh-hf-fanchen-C/tokens.txt',
-          dataDir: '',
-          noiseScale: 0.667,
-          noiseScaleW: 0.8,
-          lengthScale: 1.0
+          lexicon: 'sherpa/vits-zh-hf-fanchen-C/lexicon.txt',
+          dictDir: 'sherpa/vits-zh-hf-fanchen-C/dict'
         },
-        numThreads: 1,
-        debug: 1,
+        debug: true,
+        numThreads: 2,
         provider: 'cpu'
       },
-      ruleFsts: 'sherpa/vits-zh-hf-fanchen-C/rule.fst',
-      maxNumSentences: 1
+      maxNumStences: 1,
+      ruleFsts: [
+        'sherpa/vits-zh-hf-fanchen-C/date.fst',
+        'sherpa/vits-zh-hf-fanchen-C/phone.fst',
+        'sherpa/vits-zh-hf-fanchen-C/number.fst',
+        'sherpa/vits-zh-hf-fanchen-C/new_heteronym.fst'
+      ].join(','),
+      ruleFars: ''
     },
     asrConfig: {
       featConfig: {
@@ -30,40 +33,22 @@ export default registerAs('sherpa', () => ({
         featureDim: 80
       },
       modelConfig: {
-        transducer: {
-          encoder: '',
-          decoder: '',
-          joiner: ''
-        },
         paraformer: {
           model: 'sherpa/sherpa-onnx-paraformer-zh-2023-09-14/model.int8.onnx'
         },
-        nemoCtc: {
-          model: ''
-        },
-        whisper: {
-          encoder: '',
-          decoder: '',
-          language: '',
-          task: ''
-        },
-        tdnn: {
-          model: ''
-        },
         tokens: 'sherpa/sherpa-onnx-paraformer-zh-2023-09-14/tokens.txt',
-        numThreads: 1,
-        debug: 0,
+        numThreads: 2,
         provider: 'cpu',
-        modelType: 'paraformer'
-      },
-      lmConfig: {
-        model: '',
-        scale: 1.0
-      },
-      decodingMethod: 'greedy_search',
-      maxActivePaths: 4,
-      hotwordsFile: '',
-      hotwordsScore: 1.5
+        debug: 1
+      }
+    },
+    punctConfig: {
+      model: {
+        ctTransformer: 'sherpa/sherpa-onnx-punct-ct-transformer-zh-en-vocab272727-2024-04-12/model.onnx',
+        debug: true,
+        numThreads: 1,
+        provider: 'cpu'
+      }
     }
   }
 }))
