13b9a499257c8bf92a56eb8fa3bb0a35bd793587
DbPtZn <261849747@qq.com>
2024-10-25 09:53:15 +0800

    行内快捷键

---
+++ packages/client/src/editor/plugins/shortcut/ShotcutMenu.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import { inject, onMounted, onUnmounted, reactive, ref, useTemplateRef } from 'vue'
+import { useDialog, useMessage, useThemeVars } from 'naive-ui'
+import { Layout, textAlignFormatter } from '@textbus/editor'
+import { ComponentInstance, Selection, Injector, Renderer, fromEvent, Commander, Keyboard } from '@textbus/core'
+import _ from 'lodash'
+
+
+const injector = inject('injector') as Injector
+const themeVars = useThemeVars()
+const dialog = useDialog()
+const message = useMessage()
+
+const renderer = injector.get(Renderer)
+const commander = injector.get(Commander)
+const selection = injector.get(Selection)
+const keyboard = injector.get(Keyboard)
+
+const menuEl = useTemplateRef<HTMLElement>('menuEl')
+
+keyboard.addZenCodingInterceptor({
+  match: function (content: string): boolean {
+    return content === '/'
+  },
+  try: function (key: string): boolean {
+    console.log(key)
+    return key === '/'
+  },
+  action: function (content: string): boolean {
+    throw new Error('Function not implemented.')
+  }
+})
+
+
+</script>
+
+<template>
+  <div ref="menuEl" class="shotcut-menu">
+  
+    
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
